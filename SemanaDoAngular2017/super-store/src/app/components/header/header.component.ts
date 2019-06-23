bc261a5585c61dac74d3acb95e82478f21869565
axell-brendow <breno.axel@gmail.com>
2019-06-22 22:50:17 -0300

    Trabalho o componente menu responsivo

--- SemanaDoAngular2017/super-store/src/app/components/header/header.component.ts
+++ SemanaDoAngular2017/super-store/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/providers/menu.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public menu: MenuService) { }
 
   ngOnInit() {
   }
--- SemanaDoAngular2017/super-store/src/app/providers/menu.service.ts
+++ SemanaDoAngular2017/super-store/src/app/providers/menu.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import * as $ from 'jquery';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
+  opened: boolean = false;
+
   constructor() { }
+
+  fix()
+  {
+    if (document.body.scrollTop > 65)
+    {
+      $('section.outer').addClass('margin-fix');
+    }
+
+    else
+    {
+      $('section.outer').removeClass('margin-fix');
+    }
+  }
+
+  open()
+  {
+    this.opened = true;
+    this.fix();
+  }
+
+  close()
+  {
+    this.opened = false;
+  }
 }
