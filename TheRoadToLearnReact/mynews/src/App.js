a1f77c573c8b95cec5bc7a9022a1405912a628f8
axell-brendow <breno.axel@gmail.com>
2019-07-03 20:51:11 -0300

    Composição de componentes

--- TheRoadToLearnReact/mynews/src/App.js
+++ TheRoadToLearnReact/mynews/src/App.js
@@ -21,6 +21,54 @@ const list = [
   },
 ];
 
+class Search extends React.Component
+{
+  render()
+  {
+    const { value, onChange, children } = this.props;
+
+    return (
+      <form>
+        { children }
+        <input type="text" value={value} onChange={onChange}></input>
+      </form>
+    );
+  }
+}
+
+class Table extends React.Component
+{
+  render()
+  {
+    const { list, pattern, onDismiss } = this.props;
+
+    return (
+      list.filter(
+        filterByTerm(pattern)
+      )
+      .map(
+        (item) =>
+        <div key={ item.objectID }>
+          <span> <a href={ item.url }>{ item.title }</a> </span>
+          <span>{ item.author }</span>
+          <span>{ item.num_comments }</span>
+          <span>{ item.points }</span>
+          <span>
+            <button onClick={ () => onDismiss(item.objectID) } type="button">
+              Dismiss
+            </button>
+          </span>
+        </div>
+      )
+    );
+  }
+}
+
+function filterByTerm(searchTerm)
+{
+  return (item) => item.title.toLocaleLowerCase().includes( searchTerm.toLocaleLowerCase() );
+}
+
 class App extends React.Component
 {
   constructor(props)
@@ -28,10 +76,12 @@ class App extends React.Component
     super(props);
 
     this.state = {
-      list: list
+      list: list,
+      searchTerm: ''
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id)
@@ -43,26 +93,21 @@ class App extends React.Component
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event)
+  {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render()
   {
+    const {list, searchTerm} = this.state;
+
     return (
       <div className="App">
-        {
-          this.state.list.map(
-            (item) =>
-            <div key={ item.objectID }>
-              <span> <a href={ item.url }>{ item.title }</a> </span>
-              <span>{ item.author }</span>
-              <span>{ item.num_comments }</span>
-              <span>{ item.points }</span>
-              <span>
-                <button onClick={ () => this.onDismiss(item.objectID) } type="button">
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          )
-        }
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}></Table>
       </div>
     );
   }
