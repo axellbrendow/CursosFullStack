4a897d75e95237e599baeef0fe54165a864e0de4
axell-brendow <breno.axel@gmail.com>
2019-07-03 01:35:14 -0300

    Listagem simples

--- TheRoadToLearnReact/mynews/src/App.js
+++ TheRoadToLearnReact/mynews/src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        list.map(
+          (item) =>
+          <div key={ item.objectID }>
+            <span> <a href={ item.url }>{ item.title }</a> </span>
+            <span>{ item.author }</span>
+            <span>{ item.num_comments }</span>
+            <span>{ item.points }</span>
+          </div>
+        )
+      }
     </div>
   );
 }
