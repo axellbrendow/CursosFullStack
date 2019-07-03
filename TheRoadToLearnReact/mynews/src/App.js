698cc24fa3a0dc2857ff264b3eb940c6585fdb0d
axell-brendow <breno.axel@gmail.com>
2019-07-03 01:50:28 -0300

    Listagem simples por classe

--- TheRoadToLearnReact/mynews/src/App.js
+++ TheRoadToLearnReact/mynews/src/App.js
@@ -21,23 +21,35 @@ const list = [
   },
 ];
 
-function App()
+class App extends React.Component
 {
-  return (
-    <div className="App">
-      {
-        list.map(
-          (item) =>
-          <div key={ item.objectID }>
-            <span> <a href={ item.url }>{ item.title }</a> </span>
-            <span>{ item.author }</span>
-            <span>{ item.num_comments }</span>
-            <span>{ item.points }</span>
-          </div>
-        )
-      }
-    </div>
-  );
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      list: list
+    };
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        {
+          this.state.list.map(
+            (item) =>
+            <div key={ item.objectID }>
+              <span> <a href={ item.url }>{ item.title }</a> </span>
+              <span>{ item.author }</span>
+              <span>{ item.num_comments }</span>
+              <span>{ item.points }</span>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
