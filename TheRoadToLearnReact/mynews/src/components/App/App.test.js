5d04030e6ce8531a4cf153ce88a59aefd17e0f04
axell-brendow <breno.axel@gmail.com>
2019-07-06 19:54:08 -0300

    Snapshot Tests com Jest

--- TheRoadToLearnReact/mynews/src/components/App/App.test.js
+++ TheRoadToLearnReact/mynews/src/components/App/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from '.';
+import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App',
+  () =>
+  {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('has a valid snapshot',
+      () =>
+      {
+        const component = renderer.create( <App/> );
+        const tree = component.toJSON();
+
+        expect(tree).toMatchSnapshot();
+      }
+    );
+  }
+);
