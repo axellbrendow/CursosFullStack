e08a241bd914600826c2bb763cb2622f82785ed9
axell-brendow <breno.axel@gmail.com>
2019-12-09 15:14:16 -0300

    Implementa múltiplos Contexts com Hooks

--- react-context-and-hooks/src/components/BookList/index.js
+++ react-context-and-hooks/src/components/BookList/index.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {ThemeContext} from '../../contexts/ThemeContext';
 
-export default class BookList extends Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        const bookListStyle = {
-            background: theme.bookListBg,
-            color: theme.textColor
-        };
-        const itemsStyle = { background: theme.navBg }
-        
-        return (
-            <div className="BookList" style={bookListStyle}>
-                <ul>
-                    <li style={itemsStyle}>The way of kings</li>
-                    <li style={itemsStyle}>The name of the wind</li>
-                    <li style={itemsStyle}>The final empire</li>
-                </ul>
-            </div>
-        );
-    }
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    const bookListStyle = {
+        background: theme.bookListBg,
+        color: theme.textColor
+    };
+    const itemsStyle = { background: theme.navBg }
+    
+    return (
+        <div className="BookList" style={bookListStyle}>
+            <ul>
+                <li style={itemsStyle}>The way of kings</li>
+                <li style={itemsStyle}>The name of the wind</li>
+                <li style={itemsStyle}>The final empire</li>
+            </ul>
+        </div>
+    );
 }
+
+export default BookList;
--- react-context-and-hooks/src/components/Navbar/index.js
+++ react-context-and-hooks/src/components/Navbar/index.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {ThemeContext} from '../../contexts/ThemeContext';
 import { AuthContext } from '../../contexts/AuthContext';
 
-export default class Navbar extends Component {
-    // static contextType = ThemeContext;
+const Navbar = () => {
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    const { isLightTheme, light, dark, toggleTheme } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    const navStyle = {
+        background: theme.navBg,
+        color: theme.textColor
+    };
+    
+    return (
+        <nav className="Navbar" style={navStyle}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged in' : 'Logged out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li onClick={toggleTheme}>Change Theme</li>
+            </ul>
+        </nav>
+    );
+};
 
-    render() {
-        return (
-            <AuthContext.Consumer>{ (authContext) => (
-                <ThemeContext.Consumer>{ (themeContext) => {
-                    const { isAuthenticated, toggleAuth } = authContext;
-                    const { isLightTheme, light, dark, toggleTheme } = themeContext;
-                    const theme = isLightTheme ? light : dark;
-                    const navStyle = {
-                        background: theme.navBg,
-                        color: theme.textColor
-                    };
-                    
-                    return (
-                        <nav className="Navbar" style={navStyle}>
-                            <h1>Context App</h1>
-                            <div onClick={toggleAuth}>
-                                {isAuthenticated ? 'Logged in' : 'Logged out'}
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                                <li onClick={toggleTheme}>Change Theme</li>
-                            </ul>
-                        </nav>
-                    );
-                }}
-                </ThemeContext.Consumer>
-            )}
-            </AuthContext.Consumer>
-        );
-    }
-}
+export default Navbar;
