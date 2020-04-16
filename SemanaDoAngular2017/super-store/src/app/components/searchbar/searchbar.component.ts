4db5ee53fbd5a29adadfb489aa69d0d26368f815
axellbrendow <axellbrendow@ioasys.com.br>
2020-04-15 21:16:45 -0300

    Focus input when modal show up

--- SemanaDoAngular2017/super-store/src/app/components/searchbar/searchbar.component.ts
+++ SemanaDoAngular2017/super-store/src/app/components/searchbar/searchbar.component.ts
@@ -1,24 +1,48 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Input,
+  OnDestroy
+} from '@angular/core';
 import { ProductsService } from '../../providers/products.service';
 import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.sass']
 })
-export class SearchbarComponent implements OnInit {
+export class SearchbarComponent implements OnInit, OnDestroy {
 
   submitted: boolean = false;
   keyword: string = null;
   items: any[] = [];
+  openEventSubscription: Subscription;
   // @Output é um decorator https://ultimatecourses.com/blog/angular-decorators
   // https://blog.wgbn.com.br/fazendo-o-typescript-trabalhar-para-voc%C3%AA-546bd08a93ac
   @Output() closeSearch = new EventEmitter();
+  @Input() openEvent: Observable<void>;
+  @ViewChild('productName', { static: false }) myInput: ElementRef;
 
   constructor(public products: ProductsService, public router: Router) { }
 
   ngOnInit() {
+    this.openEventSubscription = this.openEvent.subscribe(() => this.focusInput());
+  }
+
+  ngOnDestroy() {
+    this.openEventSubscription.unsubscribe();
+  }
+
+  focusInput() {
+    setTimeout(() => {
+      this.myInput.nativeElement.focus();
+    }, 0);
   }
 
   submit(e) {
