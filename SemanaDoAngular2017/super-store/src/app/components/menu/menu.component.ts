dea70a04704590cd050cb32864bd8a03fc4d7787
axellbrendow <axellbrendow@ioasys.com.br>
2020-04-15 21:16:09 -0300

    Add modal to menu

--- SemanaDoAngular2017/super-store/src/app/components/menu/menu.component.ts
+++ SemanaDoAngular2017/super-store/src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from 'src/app/providers/menu.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -8,9 +9,24 @@ import { MenuService } from 'src/app/providers/menu.service';
 })
 export class MenuComponent implements OnInit {
 
+  public openEventSubject = new Subject<void>();
+
+  public modal: boolean = false;
+
   constructor(public menu: MenuService) { }
 
   ngOnInit() {
+  }
+
+  showSearch()
+  {
+    this.modal = true;
+    this.openEventSubject.next();
+  }
+
+  hideSearch()
+  {
+    this.modal = false;
   }
 
 }
